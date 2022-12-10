028cec87341febe95fb31edf4e918ba70658b755
Adrian Vinicius <115122220+AdrVinicius@users.noreply.github.com>
2022-12-10 13:39:52 -0300

    Create exercicio18.java

---
+++ Exercicio18/exercicio18.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicio18 {
+    public static void main(String[] args) {
+
+        double tamanhoArquivo;
+        double velocidadeLink;
+        double tempoEmSegundos;
+        double tempoEmMinutos;
+
+        Scanner teclado = new Scanner(System.in);
+        tamanhoArquivo = teclado.nextDouble();
+        velocidadeLink = teclado.nextDouble();
+        teclado.close();
+
+        
+        tamanhoArquivo = tamanhoArquivo * 8;
+       
+        tempoEmSegundos = tamanhoArquivo / velocidadeLink;
+       
+        tempoEmMinutos = tempoEmSegundos / 60;
+
+        
+        System.out.format("o tempo em minutos é %.2f", + tempoEmMinutos);
+    }
+}
